a828d71b94a924b16b74b3b8344c1d9b27adfc40
Chengli Xu <84907289+xiaoxuinchina@users.noreply.github.com>
2026-08-14 11:12:34 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE='xcl-workbench-v2';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.json',
+  './xcl_avatar.jpg',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+));
+self.addEventListener('fetch',e=>e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+));
